3dd6a9f88bedd431536f36c077a74ff8c5595424
Toivonen Ropi <s2001078@edu.bc.fi>
2022-12-02 12:21:45 +0200

    Update taso.js

--- js/taso.js
+++ js/taso.js
@@ -5,8 +5,8 @@ const TASO = {
     "pelaajanAlkuHP": 20,
     "pelaajanMaksimiHP": 100,
     "pisterajaYhdelleHplle": 100,
-    "pelinAloitushuoneenNro": 8,
-    "pelinLoppuhuoneenNro": 9,
+    "pelinAloitushuoneenNro": 1,
+    "pelinLoppuhuoneenNro": 13,
     "tekstit": {
         "pohjoinen":"Pohjoinen",
         "ita":"Itä",
@@ -26,11 +26,11 @@ const TASO = {
     "huoneet": [
         {
             "huoneNro": 1,
-            "huoneteksti": "Heräät sängystäsi nälkäisenä. Puet vaatteet päällesi ja päätät lähteä ulos syömään.", 
+            "huoneteksti": "Heräät sängystäsi nälkäisenä. Puet vaatteet päällesi ja päätät lähteä ulos.", 
             "huonehp": {
                 "hp": 10, 
                 "tekstit": {
-                    "vaikutus": "Olet ulkona. Tajusit ettei sinulla ole rahaa! Päätät lähteä pullojen etsintään ", 
+                    "vaikutus": "", 
                     "loppu": "kaikki voimat käytetty"
                 }, 
                 "toistoLkm": 2
@@ -45,167 +45,244 @@ const TASO = {
         },
         {
             "huoneNro": 2,
-            "huoneteksti": "Olet isossa aulassa. Aulan länsiseinällä ja itäseinällä on ovi. Pohjoisseinällä aukeaa portaikko yläkertaan. Portaiden yläpäässä on porrastasanne.",
+            "huoneteksti": "Lähdet kävelemään ja tajuat että olet nälkäinen. Sinulla ei kuitenkaan ole rahea ruokaan. Etsi pulloja, yksi pullo on yksi rahe, tarvitset 8 rahea.",
             "huonehp": null,
-            "pohjoinen": {"huoneeseen": 7, "avain": null},
-            "ita": {"huoneeseen": 4, "avain": null},
+            "pohjoinen": {"huoneeseen": 4, "avain": null},
+            "ita": {"huoneeseen": 3, "avain": null},
             "etela": {"huoneeseen": 1, "avain": null},
-            "lansi": {"huoneeseen": 3, "avain": null},
-            "vastustaja": {
-                "nimi": "Robotti",
-                "teksti": "TR3-sarjan vartijarobotti seisoo keskellä lattiaa. Robotin sivulokerossa on avain.",
-                "voittoviesti": "Voitit vartijarobotin. Saat lisää kokemuspisteitä.",
-                "tappioviesti": "Hävisit ottelun. Kuntosi sai kolauksen.",
-                "pisteet": 100,
-                "hp": 10,
-                "voittoraja": 10
-            },
+            "lansi": {"huoneeseen": 5, "avain": null},
+            "vastustaja": null,
             "avain": {
-                "nimi": "avain",
+                "nimi": "",
                 "numero": 1,
-                "teksti": "Avain kimmeltää kultaisena.",
-                "vaikutus": "Avain näyttäisi sopivan vanhaan riippulukkoon!",
-                "pisteet": 150,
+                "teksti": "",
+                "vaikutus": "",
+                "pisteet": 0,
                 "hp": 0
             },
             "esine": null         
         },
         {
             "huoneNro": 3,
-            "huoneteksti": "Täällä lemahtaa! Huone on selvästi toiminut jonkinlaisena varastona. Tämä on umpikuja. Pääset ainosastaan takaisin aulaan.",
+            "huoneteksti": "Löysit yhden pullon.",
             "huonehp": null,
-            "pohjoinen": null,
-            "ita": {"huoneeseen": 2, "avain": null},
+            "pohjoinen": {"huoneeseen": 6, "avain": null},
+            "ita":  null,
             "etela": null,
-            "lansi": null,
+            "lansi":{"huoneeseen": 2, "avain": null},
             "vastustaja": null,
             "avain": null,
             "esine": {
-                "nimi": "energiajuoma",
-                "teksti": "Takaseinällä olevassa hyllyssä on pölyinen energiajuomapullo",
-                "vaikutus": "ilmeisesti juoma oli pilaantunutta ja siitä aiheutui vatsanväänteitä",
-                "pisteet": 0,
+                "nimi": "pullo",
+                "teksti": "",
+                "vaikutus": "Löysit yhden pullon.",
+                "pisteet": 1,
                 "hp": -5
-            }            
-        },
-        {
-            "huoneNro": 4,
-            "huoneteksti": "Olet pienessä sivuhuoneessa. Pohjoisseinällä on hissi yläkertaan. Eteläseinässä on sortuma. Sortuma paljastaa salahuoneen, jonne voi siirtyä seinässä olevan reiän kautta",
-            "huonehp": null,
-            "pohjoinen": {"huoneeseen": 5, "avain": null},
-            "ita": null,
-            "etela": {"huoneeseen": 6, "avain": null},
-            "lansi": {"huoneeseen": 2, "avain": null},
-            "vastustaja": null,
-            "avain": {
-                "nimi": "taskulamppu",
-                "numero": 2,
-                "teksti": "Lattialla on taskulamppu, jolla voit valaista tietäsi",
-                "vaikutus": "Nyt voi mennä myös pimeisiin huoneisiin.",
-                "pisteet": 200,
-                "hp": 0
+            }},
+            {
+                "huoneNro": 4,
+                "huoneteksti": "Ei pulloja, jatka etsimistä!",
+                "huonehp": null,
+                "pohjoinen":  null,
+                "ita":{"huoneeseen": 8, "avain": null}, 
+                "etela": {"huoneeseen": 2, "avain": null},
+                "lansi": null,
+                "vastustaja": null,
+                "avain": {
+                    "nimi": "taskulamppu",
+                    "numero": 2,
+                    "teksti": "Lattialla on taskulamppu, jolla voit valaista tietäsi",
+                    "vaikutus": "Nyt voi mennä myös pimeisiin huoneisiin.",
+                    "pisteet": 200,
+                    "hp": 0
+                },
+                "esine": null
             },
-            "esine": null
+            {
+                "huoneNro": 5,
+                "huoneteksti": "löysit kaksi pulloa.",
+                "huonehp": {
+                    "hp": 10, 
+                    "tekstit": {
+                        "vaikutus":"Huone on täynnä kultapölyä. Kullan näkeminen piristää kummasti.", 
+                        "loppu": "Kultakin loppuu aikanaan"
+                    }, 
+                    "toistoLkm": 1
+                },
+                "pohjoinen": {"huoneeseen": 7, "avain": null,
+                "ita": {"huoneeseen": 2, "avain":null,
+                "etela": null},
+                "lansi": {"huoneeseen": 11, "avain": 1},
+                "vastustaja": null,
+                "avain": null,
+                "esine": null  
+            }},
+            {
+                "huoneNro": 6,
+                "huoneteksti": "Pyydät ohikulkijalta rahea he antavat niitä 2. ",
+                "huonehp": {
+                    "hp": 0, 
+                    "tekstit": {
+                        "vaikutus":"", 
+                        "loppu": ""
+                    }, 
+                    "toistoLkm": 10
+                },
+                "pohjoinen": null,
+                "ita": null,
+                "etela": {"huoneeseen": 3, "avain":null},
+                "lansi": {"huoneeseen": 8, "avain": null},
+                "vastustaja": {
+                    "nimi": "ohikulkija",
+                    "teksti": "ohikulkija kävelee ja miettii omiaan...",
+                    "voittoviesti": "Ohikulkija antoi sinulle 2 rahea.",
+                    "tappioviesti": "kuolit",
+                    "pisteet": 2,
+                    "hp": 0,
+                    "voittoraja": 0
+                },
+                "avain": null,
+                "esine": {"nimi": "",
+                    "teksti": "Huoneen keskellä on pullo. Etiketissä lukee \"palauttaa voimia\".",
+                    "vaikutus": "",
+                    "pisteet": 0,
+                    "hp": 10
+                }
+            },
+            {
+                "huoneNro": 7,
+                "huoneteksti": "puskasta kimaltelee tölkki ja keräät sen. Löysit yhden pullon.",
+                "huonehp": null,
+                "pohjoinen": null,
+                "ita": null,
+                "etela": {"huoneeseen": 5, "avain": null},
+                "lansi": null,
+                "vastustaja": null,
+                "avain": null,
+                "esine": null
+            },
+            
+    {
+        "huoneNro": 8,
+        "huoneteksti": "kompastuit kahteen pulloon.", 
+        "huonehp": {
+            "hp": 10, 
+            "tekstit": {
+                "vaikutus": "", 
+                "loppu": "kaikki voimat käytetty"
+            }, 
+            "toistoLkm": 2
         },
+        "pohjoinen": {"huoneeseen": 9, "avain": null},
+        "ita": {"huoneeseen": 6, "avain":null},
+        "etela": null,
+        "lansi": {"huoneeseen": 4, "avain": null},
+        "vastustaja": null,
+        "avain": null,
+        "esine": null
+    },
+    {
+        "huoneNro": 9,
+        "huoneteksti": "Löydät itsesi alikulkutunnelista, ihailet grafiittia.",
+        "huonehp": null,
+        "pohjoinen": null,
+        "ita": null,
+        "etela":{"huoneeseen": 8, "avain": null},
+        "lansi": {"huoneeseen": 10, "avain": null},
+        "vastustaja": {
+            "nimi": "S.U.M.O",
+            "teksti": "S.U.M.O robotti odottaa vastustajaa.",
+            "voittoviesti": "Voitit S.U.M.O:n! Onnea! Aarre odottaa lattialla.",
+            "tappioviesti": "Hävisit ottelun. S.U.M.O on edelleen voittamaton.",
+            "pisteet": 0,
+            "hp": 90,
+            "voittoraja": 0
+        },
+        "avain": null,
+        "esine": {"nimi": "Legendaarinen aarre",
+            "teksti": "Häikäisevä seuraavan sukupolven tekstiseikkailu odottaa ottajaansa.",
+            "vaikutus": "Nyt heti pelaamaan uutta vielä julkaisematonta tekstiseikkailua!",
+            "pisteet": 100,
+            "hp": 100
+        }},
         {
-            "huoneNro": 5,
-            "huoneteksti": "Olet toisessa kerroksessa. Eteläseinässä on kultaisena kiiltävä raskas hissin ovi. Länsiseinässä oleva ovi on lukittu riippulukolla.",
+            "huoneNro": 10,
+            "huoneteksti": "Olet päässyt pasilan kuninkaaseen, voit syödä aterian 7:llä rahella.", 
             "huonehp": {
                 "hp": 10, 
                 "tekstit": {
-                    "vaikutus":"Huone on täynnä kultapölyä. Kullan näkeminen piristää kummasti.", 
-                    "loppu": "Kultakin loppuu aikanaan"
+                    "vaikutus": "xxx", 
+                    "loppu": "kaikki voimat käytetty"
                 }, 
-                "toistoLkm": 1
+                "toistoLkm": 2
             },
             "pohjoinen": null,
-            "ita": null,
-            "etela": {"huoneeseen": 4, "avain": null},
-            "lansi": {"huoneeseen": 9, "avain": 1},
+            "ita":{"huoneeseen": 9, "avain": null},
+            "etela": null,
+            "lansi": null,
             "vastustaja": null,
             "avain": null,
-            "esine": null  
+            "esine": null
         },
         {
-            "huoneNro": 6,
-            "huoneteksti": "Olet osittain sortuneessa salahuoneessa. Onkohan tämä ollut aarrekammio? Huoneen länsiseinässä on naamioitu ovi.",
+            "huoneNro": 11,
+            "huoneteksti": "Olet nyt Alepan parkkipaikalla.", 
             "huonehp": {
-                "hp": -5, 
+                "hp": 10, 
                 "tekstit": {
-                    "vaikutus":"Katosta putusi pieni kivi päähäsi. Terveytesi heikkeni hieman.", 
-                    "loppu": "kaikki kivet pudonneet"
+                    "vaikutus": "Olet ulkona. Tajusit ettei sinulla ole rahaa! Päätät lähteä pullojen etsintään ", 
+                    "loppu": "kaikki voimat käytetty"
                 }, 
-                "toistoLkm": 10
+                "toistoLkm": 2
             },
-            "pohjoinen": {"huoneeseen": 4, "avain": null},
-            "ita": null,
-            "etela": null,
-            "lansi": {"huoneeseen": 1, "avain": null},
-            "vastustaja": {
-                "nimi": "Robotti",
-                "teksti": "TR13-sarjan vartijarobotti seisoo keskellä lattiaa.",
-                "voittoviesti": "Voitit vartijarobotin. Saat lisää kokemuspisteitä.",
-                "tappioviesti": "Hävisit ottelun. Kuntosi sai kolauksen.",
-                "pisteet": 0,
-                "hp": 30,
-                "voittoraja": 10
-            },
+            "pohjoinen": {"huoneeseen": 12, "avain": null},
+            "ita":{"huoneeseen": 5, "avain": null},
+            "etela": {"huoneeseen": 13, "avain":null},
+            "lansi": null,
+            "vastustaja": null,
             "avain": null,
-            "esine": {"nimi": "elämän eliksiiri",
-                "teksti": "Huoneen keskellä on pullo. Etiketissä lukee \"palauttaa voimia\".",
-                "vaikutus": "Olosi tuntuu taas pirteämmältä",
-                "pisteet": 0,
-                "hp": 10
-            }
+            "esine": null
         },
         {
-            "huoneNro": 7,
-            "huoneteksti": "Olet porrastasanteella. Kun katsot alas etelään, näet sisääntuloaulan. Itäseinällä on patinoitunut ovi.",
-            "huonehp": null,
+            "huoneNro": 12,
+            "huoneteksti": "Umpikuja...", 
+            "huonehp": {
+                "hp": 10, 
+                "tekstit": {
+                    "vaikutus": "Olet ulkona. Tajusit ettei sinulla ole rahaa! Päätät lähteä pullojen etsintään ", 
+                    "loppu": "kaikki voimat käytetty"
+                }, 
+                "toistoLkm": 2
+            },
             "pohjoinen": null,
-            "ita": {"huoneeseen": 5, "avain": null},
-            "etela": {"huoneeseen": 2, "avain": null},
+            "ita": null,
+            "etela":{"huoneeseen": 11, "avain": null},
             "lansi": null,
             "vastustaja": null,
             "avain": null,
             "esine": null
         },
         {
-            "huoneNro": 8,
-            "huoneteksti": "Edessäsi on upea ja jännittävä seikkailu. Voit liikkua neljään ilmansuuntaan, poimia esineitä ja avaimia sekä taistella vastustajia vastaan. Aloita seikkailu siirtymällä pohjoiseen.",
-            "huonehp": null,
-            "pohjoinen": {"huoneeseen": 1, "avain": null},
-            "ita": null,
-            "etela": null,
-            "lansi": null,
-            "vastustaja": null,
-            "avain": null,
-            "esine": null
-        },
-        {
-            "huoneNro": 9,
-            "huoneteksti": "Olet kulkenut pitkän matkan viimeisen vastustajasi luo. Huoneessa on kaipaamasi aarre, mutta saadaksesi sen sinun on voitettava S.U.M.O",
-            "huonehp": null,
-            "pohjoinen": {"huoneeseen": 1, "avain": null},
-            "ita": null,
-            "etela": null,
-            "lansi": null,
-            "vastustaja": {
-                "nimi": "S.U.M.O",
-                "teksti": "S.U.M.O robotti odottaa vastustajaa.",
-                "voittoviesti": "Voitit S.U.M.O:n! Onnea! Aarre odottaa lattialla.",
-                "tappioviesti": "Hävisit ottelun. S.U.M.O on edelleen voittamaton.",
-                "pisteet": 0,
-                "hp": 90,
-                "voittoraja": 0
+            "huoneNro": 13,
+            "huoneteksti": "Olet nyt Alepassa voit ostaa megaforcen yhdellä rahella.", 
+            "huonehp": {
+                "hp": 10, 
+                "tekstit": {
+                    "vaikutus": "Olet ulkona. Tajusit ettei sinulla ole rahaa! Päätät lähteä pullojen etsintään ", 
+                    "loppu": "kaikki voimat käytetty"
+                }, 
+                "toistoLkm": 2
             },
+            "pohjoinen": {"huoneeseen": 11, "avain": null},
+            "ita": null,
+            "etela": null,
+            "lansi": null,
+            "vastustaja": null,
             "avain": null,
-            "esine": {"nimi": "Legendaarinen aarre",
-                "teksti": "Häikäisevä seuraavan sukupolven tekstiseikkailu odottaa ottajaansa.",
-                "vaikutus": "Nyt heti pelaamaan uutta vielä julkaisematonta tekstiseikkailua!",
-                "pisteet": 100,
-                "hp": 100
-            }
+            "esine": null
         }
+        
+
+       
     ]
 };
